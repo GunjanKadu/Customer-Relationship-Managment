8c226b79e7221fa3418893d615225309bfcc3eda
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-14 18:05:06 +0200

    Adding customer service and implementing it

---
+++ Web-Customer-Tracker/src/com/gk/service/CustomerService.java
@@ -0,0 +1,10 @@
+package com.gk.service;
+
+import java.util.List;
+
+import com.gk.entity.Customer;
+
+public interface CustomerService {
+	
+	public List<Customer> getCustomers();
+}
---
+++ Web-Customer-Tracker/src/com/gk/service/CustomerServiceImpl.java
@@ -0,0 +1,24 @@
+package com.gk.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.gk.dao.CustomerDao;
+import com.gk.entity.Customer;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+	@Autowired
+	private CustomerDao customerDao;
+
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		return customerDao.getCustomers();
+	}
+
+}
