9033f701587275f44e6f4312e8208bb50ec8dcee
Gunjan Kaduu <agunjan.kadu@gmail.com>
2020-04-27 20:54:56 +0200

    Adding @Before advice

--- Web-Customer-Tracker/src/com/gk/aspect/CRMLoggingAspect.java
+++ Web-Customer-Tracker/src/com/gk/aspect/CRMLoggingAspect.java
@@ -2,7 +2,9 @@ package com.gk.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -14,19 +16,31 @@ public class CRMLoggingAspect {
 	private Logger myLogger = Logger.getLogger(getClass().getName());
 
 	// setup pointcut declaration
-	@Pointcut("execution(* com.gk.springdemo.controller.*.*.(..))")
-	private void forControllerPackage() {}
-	
-	@Pointcut("execution(* com.gk.service.*.*.(..))")
-	private void forServicePackage() {}
-	
-	@Pointcut("execution(* com.gk.dao.*.*.(..))")
-	private void forDaoPackage() {}
-	
-	//Combining Point Cut declaration
-	@Pointcut("forControllerPackage||forServicePackage||forDaoPackage")
-	private void forAppFlow() {}
+	@Pointcut("execution(* com.gk.springdemo.controller.*.*(..))")
+	private void forControllerPackage() {
+	}
+
+	@Pointcut("execution(* com.gk.service.*.*(..))")
+	private void forServicePackage() {
+	}
+
+	@Pointcut("execution(* com.gk.dao.*.*(..))")
+	private void forDaoPackage() {
+	}
+
+	// Combining Point Cut declaration
+	@Pointcut("forControllerPackage()||forServicePackage()||forDaoPackage()")
+	private void forAppFlow() {
+	}
+
 	// add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		// display the methods we are calling
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("====>>> in @Before:calling method: " + theMethod);
+		// display the arguments to the method
+	}
 
 	// add @AfterReturning advice
 }
